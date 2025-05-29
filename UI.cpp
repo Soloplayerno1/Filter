64f6141f2cc3033c0c80a3c2054a9c01cc318c92
Soloplayerno1 <soloplayerno2706@gmail.com>
2025-05-29 12:20:48 +0700

    Update UI.cpp

--- UI.cpp
+++ UI.cpp
@@ -1,6 +1,12 @@
 #include "UI.h"
 
-UI::UI(int window_w, int window_h, float image_frame_w, float image_frame_h, Texture* image)
+UI::UI(int window_w, int window_h, float image_frame_w, float image_frame_h, Texture* image):
+font("Fonts/..."),
+Options[GRAYSCALE]("Gray scale", font, 40),
+Options[REFLECT]("Reflect", font, 40),
+Options[BLUR]("Blur", font, 40),
+Options[SEPIA]("SEPIA", font, 40),
+Options[EDGE]("Edge", font,40)
 {
 	this->imgFrame.x = image_frame_w;
 	this->imgFrame.y = image_frame_h;
